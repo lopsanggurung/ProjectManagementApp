fdd862b699ede5d21c6838a115b98d78dce37cbd
Lopsang Gurung <lopsanggrg@gmail.com>
2020-06-13 11:23:19 -0400

    Implement PerformanceBehaviour & UnhandledExceptionBehaviour

--- src/App.Application/DependencyInjection.cs
+++ src/App.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using App.Application.Common.Behaviours;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +10,8 @@ namespace App.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
 
             return services;
         }
