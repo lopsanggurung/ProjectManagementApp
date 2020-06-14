50c99f134ef9b7628af90f360123ed436373b31e
Lopsang Gurung <lopsanggrg@gmail.com>
2020-06-14 13:47:30 -0400

    Implement Create, Update, & Delete TodoItem

---
+++ src/App.Application/TodoItems/Commands/UpdateTodoItem/UpdateTodoItemCommandValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace App.Application.TodoItems.Commands.UpdateTodoItem
+{
+    public class UpdateTodoItemCommandValidator : AbstractValidator<UpdateTodoItemCommand>
+    {
+        public UpdateTodoItemCommandValidator()
+        {
+            RuleFor(v => v.Title)
+                .MaximumLength(200)
+                .NotEmpty();
+        }
+    }
+}
---
+++ src/App.Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommandValidator.cs
@@ -0,0 +1,7 @@
+namespace App.Application.TodoItems.Commands.UpdateTodoItemDetail
+{
+    public class UpdateTodoItemDetailCommandValidator
+    {
+        // TODO: Complete Validator
+    }
+}
