7af49cf535d162681c912984f83d19829a1774e0
Lopsang Gurung <lopsanggrg@gmail.com>
2020-06-14 11:38:03 -0400

    Implement Create, Update, & Delete TodoList

--- src/App.API/Controllers/TodoListsController.cs
+++ src/App.API/Controllers/TodoListsController.cs
@@ -1,4 +1,7 @@
 using System.Threading.Tasks;
+using App.Application.TodoLists.Commands.CreateTodoList;
+using App.Application.TodoLists.Commands.DeleteTodoList;
+using App.Application.TodoLists.Commands.UpdateTodoList;
 using App.Application.TodoLists.Queries.GetTodos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +14,33 @@ namespace App.API.Controllers
         public async Task<ActionResult<TodosVm>> Get()
         {
             return await Mediator.Send(new GetTodosQuery());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateTodoListCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, UpdateTodoListCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeleteTodoListCommand { Id = id });
+
+            return NoContent();
         }
     }
 }
